25ed493c26c00f764c6e83f39c8a5248c71af173
Peter Wandag <gadnaw@gmail.com>
2023-09-27 15:04:34 +0800

    Commented the @tailwind base to prevent the preflight of tailwind that changes the default view of HTML tags

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,14 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-gray-300">
       <h1 className="pb-2 ">Collection of React Patterns from Youtube!</h1>
       <div className="flex flex-col">
         <Link href="/screen-splitscreenpattern" className="bg-red-100 border-2 border-solid border-red-200">1.1 Split Screen Pattern</Link>
         <Link href="/screen-listitempattern" className="bg-red-100 border-2 border-solid border-red-200">1.2 List Item Pattern</Link>
-        <Link href="/screen-modal" className="bg-red-100 border-2 border-solid border-red-200">1.2 List Item Pattern</Link>
+        <Link href="/screen-modal" className="bg-red-100 border-2 border-solid border-red-200">1.3 Show Modal Pattern</Link>
       </div>
+      <button>hello</button>
     </main>
   )
 }
